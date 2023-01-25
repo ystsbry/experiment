3b023ee549be802f684ebfd36ebbbf97e668d780
DESKTOP-9PTFMOR\ybaba <jmachang368@gmail.com>
2023-01-25 22:43:04 +0900

    c# クロージャーとクラスの比較

---
+++ ComparisonOfClosureAndClassSpeeds/Program.cs
@@ -0,0 +1,65 @@
+﻿using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Attributes;
+
+namespace ComparisonOfClosureAndClassSpeeds;
+
+internal class Program
+{
+    private static void Main() => BenchmarkRunner.Run<BenchmarkTest>();
+}
+
+[MemoryDiagnoser]
+public class BenchmarkTest
+{
+
+    [Benchmark]
+    public int ClassBenchmark()
+    {
+        var TargetClass = new ComparisionTargetClass();
+
+        for (var i = 0; i < 10; i++)
+        {
+            TargetClass.Increment();
+        }
+        return TargetClass.Count;
+    }
+
+    [Benchmark]
+    public int ClosureBenchmark()
+    {
+        var (IncrementClosure, GetCountClosure) = ComparisionTargetClosure.Get();
+
+        for (var i = 0; i < 10; i++)
+        {
+            IncrementClosure();
+        }
+        return GetCountClosure();
+    }
+}
+
+public class ComparisionTargetClass
+{
+    public int Count = 0;
+
+    public void Increment()
+    {
+        Count++;
+    }
+}
+
+public class ComparisionTargetClosure
+{
+    static public (Action, Func<int>) Get()
+    {
+        int count = 0;
+        return (
+        () => 
+        {
+            count++;
+        },
+        () =>
+        {
+            return count;
+        });
+    }
+}
